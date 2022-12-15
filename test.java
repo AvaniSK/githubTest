906389d9b697b2b748585332cd318fd65def1016
AvaniSK <avanipatelsask1@gmail.com>
2022-12-14 18:09:22 -0600

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,17 @@
 //this is a test file
+
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    public static void displayNames()
+    {
+        string a = "avani";
+        return a;
+    }
+}
